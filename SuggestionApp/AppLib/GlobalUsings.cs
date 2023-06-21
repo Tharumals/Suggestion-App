a22aaead00addbac7461262ef3318b136fef38ce
Tharumal Wimalarathna <stharumal95@gmail.com>
2023-06-21 18:14:47 +0530

    added models

--- SuggestionApp/AppLib/GlobalUsings.cs
+++ SuggestionApp/AppLib/GlobalUsings.cs
@@ -1,1 +1,2 @@
-﻿
+﻿global using MongoDB.Bson;
+global using MongoDB.Bson.Serialization.Attributes;
---
+++ SuggestionApp/AppLib/Models/BasicUserModel.cs
@@ -0,0 +1,20 @@
+﻿namespace AppLib.Models
+{
+    public class BasicUserModel
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string DisplayName { get; set; }
+
+        public BasicUserModel()
+        {
+
+        }
+
+        public BasicUserModel(UserModel userModel)
+        {
+            Id= userModel.Id;
+            DisplayName= userModel.DisplayName;
+        }
+    }
+}
